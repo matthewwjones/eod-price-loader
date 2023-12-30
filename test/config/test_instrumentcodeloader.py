9263428e796f55007692f79a786eb90d5f9c51e6
Matt Jones <matthew.w.jones@me.com>
2023-12-30 19:02:16 +0000

    Add instrument code loader and tests.

--- test/config/test_instrumentcodeloader.py
+++ test/config/test_instrumentcodeloader.py
@@ -5,7 +5,18 @@ from config.instrumentcodeloader import InstrumentCodeLoader
 
 class TestInstrumentCodeLoader(TestCase):
     test_instrument_codes_file = 'test/config/test-data/test-instrument-codes.txt'
+    empty_instrument_codes_file = 'test/config/test-data/empty-instrument-codes-file.txt'
 
     def test_load_instrument_codes_returns_expected_list(self):
         loader = InstrumentCodeLoader(self.test_instrument_codes_file)
         self.assertEqual(loader.load_instrument_codes(), ['HSBA.LSE', 'OCDO.LSE', 'VOD.LSE'])
+
+    def test_load_instrument_codes_empty_file(self):
+        loader = InstrumentCodeLoader(self.empty_instrument_codes_file)
+        self.assertEqual(loader.load_instrument_codes(), [])
+
+    def test_load_instrument_codes_returns_expected_list2(self):
+        loader = InstrumentCodeLoader('invalid-path')
+        with self.assertRaises(Exception) as context:
+            loader.load_instrument_codes()
+        self.assertIn('File invalid-path does not exist.', str(context.exception))
