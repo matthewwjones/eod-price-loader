7bf28ea0e67632f45a75ffbee617a67ed0597771
Matt Jones <matthew.w.jones@me.com>
2024-01-22 16:33:02 +0000

    Restructure, add fetch from EOD HD. (#7)
    * Restructure, add fetch from EOD HD.
    
    * Test fixes.
    
    * Add requirements.txt
    
    * Fix paths.

--- src/config/apiconfigloader.py
+++ src/config/apiconfigloader.py
@@ -8,11 +8,11 @@ class ApiConfigLoader:
         self.api_config_file = api_config_file
 
     def load_api_config(self):
-        self.log.info(f"Loading API config from {self.api_config_file}...")
+        self.log.info(f"Loading API config from {self.api_config_file}")
         try:
             with open(self.api_config_file) as file:
                 for line in file:
                     if line.startswith('api_token='):
-                        return line.rstrip('api_token=')
+                        return line.replace('api_token=', '')
         except FileNotFoundError as e:
-            raise Exception(f"API config file {self.api_config_file} does not exist.", e)
+            raise Exception(f'API config file {self.api_config_file} does not exist. {e}')
--- test/load/test_eodloader.py
+++ test/load/test_eodloader.py
@@ -4,10 +4,12 @@ from src.load.eodloader import EodLoader
 
 
 class TestEodLoader(TestCase):
-    json = '[{"date":"2023-10-31","open":169.35,"high":170.9,"low":167.9,"close":170.77,"adjusted_close":170.77,' \
-           '"volume":44846000}] '
+    json = [{
+        "date": "2023-10-31", "open": 169.35, "high": 170.9, "low": 167.9, "close": 170.77, "adjusted_close": 170.77,
+        "volume": 44846000}
+    ]
 
     def test_extract_close_from_response_returns_expected_value(self):
-        loader = EodLoader([])
+        loader = EodLoader(None, [])
         close_price = loader.extract_close_from_response(self.json)
         self.assertEqual(close_price, 170.77)
