ecf70facc380a5925ba24b658b982e9ce600be0b
Matt Jones <matthew.w.jones@me.com>
2024-01-22 16:30:21 +0000

    Fix paths.

--- test/config/test_apiconfigloader.py
+++ test/config/test_apiconfigloader.py
@@ -4,7 +4,7 @@ from src.config.apiconfigloader import ApiConfigLoader
 
 
 class TestApiConfigLoader(TestCase):
-    test_api_config_file = '../test/config/test-data/test-api-config.txt'
+    test_api_config_file = 'test/config/test-data/test-api-config.txt'
 
     def test_load_api_config_returns_expected_key(self):
         loader = ApiConfigLoader(self.test_api_config_file)
